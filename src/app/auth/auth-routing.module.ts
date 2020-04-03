c49f460f33b74987d015349e4dde946704f93410
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-04-03 14:30:01 +0200

    Fix Router settings

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AuthRoutingModule { }
