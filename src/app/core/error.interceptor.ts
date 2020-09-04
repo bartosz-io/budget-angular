beaf4f4b47e3c49a51fe91296d4ce59fac9f1ae7
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-09-04 13:30:01 +0200

    Suppress logged user query error

--- src/app/core/error.interceptor.ts
+++ src/app/core/error.interceptor.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError, of } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SnackBarComponent } from '../shared/components/snackbar/snackbar.component';
+import { config } from '../core/config';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
@@ -13,13 +14,23 @@ export class HttpErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        this.snackBar.openFromComponent(SnackBarComponent, {
-          duration: 3000,
-          data: error.error.msg ?? 'Unknown error'
-        });
-
+        // when Node backend is not running, then logged user query fails; let's not show error
+        if (!this.isQueryForLoggedUser(error)) {
+          this.showErrorMessage(error);
+        }
         return throwError(error);
       }));
+  }
+
+  private isQueryForLoggedUser(error: HttpErrorResponse) {
+    return error.url.endsWith(`${config.authUrl}/user`);
+  }
+
+  private showErrorMessage(error: HttpErrorResponse) {
+    this.snackBar.openFromComponent(SnackBarComponent, {
+      duration: 3000,
+      data: error.error.msg ?? 'Unknown error'
+    });
   }
 
 }
