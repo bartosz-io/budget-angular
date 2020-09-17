b0169d68e667f52e68b9d8c8e4eaba50fe808aca
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-09-17 15:54:39 +0200

    Add SecretDialogComponent

---
+++ src/app/settings/account/components/secret-dialog/secret-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'secret-dialog',
+  templateUrl: './secret-dialog.component.html'
+})
+export class SecretDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<SecretDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public keyuri: string) {
+  }
+
+}
