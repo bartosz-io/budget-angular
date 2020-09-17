0701b2047644c952bbf5be62fc5a236f0c61f786
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2020-09-17 15:51:22 +0200

    Add OTP support in LoginComponent

--- src/app/auth/containers/login/login.component.ts
+++ src/app/auth/containers/login/login.component.ts
@@ -2,10 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { retryWhen, tap, switchMap, filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 import { SnackBarComponent } from './../../../shared/components/snackbar/snackbar.component';
+import { OtpComponent } from '../../components/otp-dialog/otp.component';
 import { AuthService } from '../../services/auth.service';
 import { OAuthService } from './../../services/oauth.service';
+import { LoginRequest } from '@models/loginRequest';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -22,6 +28,7 @@ export class LoginComponent implements OnInit {
     private oauthService: OAuthService,
     private formBuilder: FormBuilder,
     private snackBar: MatSnackBar,
+    private dialog: MatDialog,
     private route: ActivatedRoute,
     private router: Router) { }
 
@@ -43,16 +50,34 @@ export class LoginComponent implements OnInit {
   get f() { return this.loginForm.controls; }
 
   login() {
-    this.authService.login(
-      {
-        email: this.f.email.value,
-        password: this.f.password.value
-      }
+    const loginRequest: LoginRequest = {
+      email: this.f.email.value,
+      password: this.f.password.value
+    };
+
+    this.authService.login(loginRequest).pipe(
+      retryWhen(this.invalidOtp(loginRequest))
     ).subscribe((user) => this.router.navigate([this.authService.getInitialPathForRole(user.role)]));
   }
 
   getIdToken() {
     this.oauthService.requestIdToken();
+  }
+
+  private invalidOtp(loginRequest: LoginRequest) {
+    return (errors: Observable<HttpErrorResponse>) => errors.pipe(
+      filter(err => err.error.msg === 'OTP_REQUIRED'),
+      switchMap(() => this.requestOtp()),
+      tap(otp => loginRequest.otp = otp)
+    );
+  }
+
+  private requestOtp() {
+    const config = {
+      width: '400px',
+      disableClose: true
+    };
+    return this.dialog.open(OtpComponent, config).afterClosed();
   }
 
 }
