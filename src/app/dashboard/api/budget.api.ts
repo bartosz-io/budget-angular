4fac41aaa1340c69692d6fa97e5fa5c5d8472a77
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2021-11-03 18:16:50 +0100

    Use ConfigProvider in BudgetApi

--- src/app/dashboard/api/budget.api.ts
+++ src/app/dashboard/api/budget.api.ts
@@ -6,20 +6,24 @@ import { map } from 'rxjs/operators';
 import { BudgetSummary } from '@models/budgetSummary';
 import { Budget } from '@models/budget';
 import { Period } from '@models/period';
-import { config } from '../../core/config';
+import { ConfigProvider } from '../../core/config.provider';
 
 @Injectable()
 export class BudgetApi {
 
-  constructor(private http: HttpClient) {}
+  readonly apiUrl: string;
+
+  constructor(private http: HttpClient, configProvider: ConfigProvider) {
+    this.apiUrl = configProvider.getConfig().apiUrl;
+  }
 
   getBudgets(period: Period): Observable<Budget[]> {
-    return this.http.get(`${config.apiUrl}/budgets/?month=${period.month}&year=${period.year}`)
+    return this.http.get(`${this.apiUrl}/budgets/?month=${period.month}&year=${period.year}`)
       .pipe(map((budgets: any[]) => budgets.map(budget => Budget.buildFromJson(budget))));
   }
 
   getBudgetSummary(period: Period): Observable<BudgetSummary> {
-    return this.http.get<BudgetSummary>(`${config.apiUrl}/budget-summary/?month=${period.month}&year=${period.year}`)
+    return this.http.get<BudgetSummary>(`${this.apiUrl}/budget-summary/?month=${period.month}&year=${period.year}`)
       .pipe(map(summary => BudgetSummary.buildFromJson(summary)));
   }
 
