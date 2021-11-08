7529a353b6446b98cca85689f5ee4efca2cd071d
Bartosz Pietrucha <pietrucha.bartosz@gmail.com>
2021-11-08 18:56:02 +0100

    Add dashboard component tests

---
+++ src/app/dashboard/containers/dashboard/dashboard.component.deep.spec.ts
@@ -0,0 +1,47 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+
+import { ConfigProvider } from './../../../core/config.provider';
+import { SummaryComponent } from './../../components/summary/summary.component';
+import { BudgetProgressComponent } from './../../components/budget-progress/budget-progress.component';
+import { DashboardComponent } from './dashboard.component';
+import { PeriodService } from './../../../shared/services/period.service';
+import { DashboardService } from './../../dashboard.service';
+import { CacheService } from './../../../core/cache.service';
+import { BudgetApi } from './../../api/budget.api';
+
+describe('Deep test: DashboardComponent', () => {
+
+  let fixture: ComponentFixture<DashboardComponent>;
+  let component: DashboardComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DashboardComponent,
+        BudgetProgressComponent,
+        SummaryComponent
+      ],
+      imports: [
+        HttpClientModule,
+        MatCardModule,
+        MatProgressSpinnerModule
+      ],
+      providers: [
+        DashboardService,
+        PeriodService,
+        BudgetApi,
+        ConfigProvider,
+        CacheService,
+      ],
+    });
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('creates the component', () => {
+    expect(component).toBeTruthy();
+  });
+});
